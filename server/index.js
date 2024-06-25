b27eb1a3c6597ba973cce8037b74be868cac2c8d
ani116 <137396508+ani116@users.noreply.github.com>
2024-06-25 17:39:37 +0530

    changes in indexjs

--- server/index.js
+++ server/index.js
@@ -25,7 +25,8 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors(
     {
-        origin: "http://localhost:3000"    // the request coming from frontend, we have to entertaint it
+        origin: "*",    // the request coming from frontend, we have to entertaint it
+        credentials: true,
     }
 ))
 
